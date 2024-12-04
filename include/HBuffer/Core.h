99ca33676168dfe709b0062e2d79aad5682c9d79
1WeirdDev <the1weirddev@gmail.com>
2024-12-04 15:55:37 -0600

    Update

--- include/HBuffer/Core.h
+++ include/HBuffer/Core.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#undef HBUFFER_USE_PCH
 //Handles defines and importing standard library
 #ifdef HBUFFER_USE_PCH
 #ifndef HBUFFER_PCH_DIR
@@ -10,6 +10,7 @@
 else
 
 #include <string>
+#include <string.h>
 #include <map>
 #include <memory>
 
