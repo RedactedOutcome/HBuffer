4dea46eb14058fbc6f409a900df2f387e97e83d5
1WeirdDev <the1weirddev@gmail.com>
2024-12-07 10:55:15 -0600

    src

---
+++ src/Example1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "HBuffer/HBuffer.hpp"
+
+int main(int argc, char** argv){
+    HBuffer hBuffer = "HEllo WOrld";
+
+    std::cout << hBuffer.GetCStr() << std::endl;
+    std::cout << "HBuffer " << (hBuffer.StartsWith("HEl") ? "does" : "doesn't") << " start with Hel"<<std::endl;
+    std::cout << "HBuffer " << (hBuffer.StartsWith("Hll") ? "does" : "doesn't") << " start with Hll"<<std::endl;
+    std::cout << "HBuffer string at 4 with 5 characters is " << hBuffer.SubString(4, 5).GetCStr()<<std::endl;
+}
