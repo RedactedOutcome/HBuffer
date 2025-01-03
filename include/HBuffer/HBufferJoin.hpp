077dd0e70b40fef31164299e33b94b41ba46e4c0
The1WeirdDev <the1weirddev@gmail.com>
2025-01-03 07:33:25 -0600

    Fixed HBufferJoin Get

--- include/HBuffer/HBufferJoin.hpp
+++ include/HBuffer/HBufferJoin.hpp
@@ -316,7 +316,7 @@ public:
     /// @return returns the character at i. returns '\0' if out of range of buffers
     char Get(size_t i) const HBUFF_NOEXCEPT{
         if(i <= m_Buffer1.m_Size)return m_Buffer1.m_Data[i];
-        if(i >= m_Buffer1.m_Size + m_Buffer2.m_Size)return false;
+        if(i >= m_Buffer1.m_Size + m_Buffer2.m_Size)return '\0';
         return m_Buffer2.m_Data[i - m_Buffer1.m_Size];
     }
 public:
