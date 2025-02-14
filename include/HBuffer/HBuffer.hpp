4558e52426908ea7b3d557122d035ca9f8de40b3
The1WeirdDev <the1weirddev@gmail.com>
2025-02-14 12:08:59 -0600

    Update

--- include/HBuffer/HBuffer.hpp
+++ include/HBuffer/HBuffer.hpp
@@ -29,7 +29,7 @@ public:
     /// @param the capacity of the buffer
     /// @param canFree 
     /// @param canModify decides if the buffer can directly modify data or if it has to make a copy if it needs to edit data
-    HBuffer(const char* str, size_t len, size_t capacity, bool canFree, bool canModify)HBUFF_NOEXCEPT:m_Size(len), m_Data(const_cast<char*>(str)), m_Capacity(capacity), m_CanFree(canFree), m_CanModify(canModify){}
+    explicit HBuffer(char* str, size_t len, size_t capacity, bool canFree, bool canModify)HBUFF_NOEXCEPT:m_Size(len), m_Data(str), m_Capacity(capacity), m_CanFree(canFree), m_CanModify(canModify){}
 
     /// @brief Makes the buffer an exact non owning copy of param buffer
     /// @param buffer 
