c530392506b1f56b943090a975a9b4eb81e1ab4d
The1WeirdDev <the1weirddev@gmail.com>
2025-03-14 16:37:53 -0500

    SubBuffer Temp Fix

--- include/HBuffer/HBuffer.hpp
+++ include/HBuffer/HBuffer.hpp
@@ -569,7 +569,7 @@ public:
     /// @param len the amount of characters to copy to new buffer. If -1 than whole buffer. Caps out on buffer size
     HBuffer SubBuffer(size_t at, size_t len) const HBUFF_NOEXCEPT{
         HBuffer buffer;
-        buffer.m_Size = std::min(len, m_Size);
+        buffer.m_Size = std::min(m_Size - at, len);
         buffer.m_Capacity = buffer.m_Size + 1;
         buffer.m_Data = new char[buffer.m_Capacity];
 
