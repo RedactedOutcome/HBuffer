aaed665ce6b1cd50d037bfc7ac855ea66ec4832e
1WeirdDev <the1weirddev@gmail.com>
2025-04-18 11:43:07 -0500

    Update

--- include/HBuffer/HBufferExtras.hpp
+++ include/HBuffer/HBufferExtras.hpp
@@ -1,13 +1,22 @@
 #pragma once
 
-#include "HBuffer.h"
-
+#include "HBuffer.hpp"
 
 struct HBufferLowercaseHash{
-    std::size_t operator()(const HBuffer& h) const {
-        return reinterpret_cast<std::uintptr_t>(&h) % 10007;
+    std::size_t operator()(const HBuffer& buff) const {
+        std::size_t hash = 0;
+        std::size_t prime = 31; // A small prime number
+
+        char* data = buff.GetData();
+        for (size_t i = 0; i < buff.GetSize(); i++){
+            char c = data[i];
+            if(c >= 'A' && c <= 'Z')c -= ('a' - 'A');
+            hash = hash * prime + c;
+        }
+
+        return hash;
     }
     bool operator()(const HBuffer& left, const HBuffer& right) const {
         return &left == &right;
     }
-}
+};
