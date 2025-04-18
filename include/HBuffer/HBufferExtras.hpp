07a0150a541c2359953d816a8baa0f3d4a9c5144
1WeirdDev <the1weirddev@gmail.com>
2025-04-18 14:43:03 -0500

    Update

--- include/HBuffer/HBufferExtras.hpp
+++ include/HBuffer/HBufferExtras.hpp
@@ -10,13 +10,33 @@ struct HBufferLowercaseHash{
         char* data = buff.GetData();
         for (size_t i = 0; i < buff.GetSize(); i++){
             char c = data[i];
-            if(c >= 'A' && c <= 'Z')c -= ('a' - 'A');
+            if(c >= 'A' && c <= 'Z'){
+                c += ('a' - 'A');
+            }
             hash = hash * prime + c;
         }
 
         return hash;
     }
+};
+
+struct HBufferLowercaseEquals{
     bool operator()(const HBuffer& left, const HBuffer& right) const {
-        return &left == &right;
+        size_t size = left.GetSize();
+        if(size != right.GetSize())return false;
+
+        //Assuming if it has a size it has valid memory if not then the user created the buffer wrong
+        //if(!m_Data || !right.m_Data)return false;
+
+        const char* leftData = left.GetData();
+        const char* rightData = right.GetData();
+        for(size_t i = 0; i < size; i++){
+            char leftChar = leftData[i];
+            char rightChar = rightData[i];
+            if(leftChar >= 'A' && leftChar <= 'Z')leftChar += 'a' - 'A';
+            if(rightChar >= 'A' && rightChar <= 'Z')rightChar += 'a' - 'A';
+            if(leftChar != rightChar)return false;
+        }
+        return true;
     }
 };
