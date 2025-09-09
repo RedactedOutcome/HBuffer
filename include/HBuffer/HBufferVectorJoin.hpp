42adb51e696a70b104f533cba5206b0920160459
1WeirdDev <the1weirddev@gmail.com>
2025-09-09 18:08:36 -0500

    added

--- include/HBuffer/HBufferVectorJoin.hpp
+++ include/HBuffer/HBufferVectorJoin.hpp
@@ -107,7 +107,7 @@ public:
 
         size_t accumulatedLength = 0;
         for(size_t index = startIndex; index < indicesSize; index++){
-            HBuffer& referenceBuffer = m_Vectors[index];
+            const HBuffer& referenceBuffer = m_Vectors[index];
             size_t bufferAt = index == startIndex ? at - totalBefore : 0;
             size_t bufferLen = std::min(referenceBuffer.GetSize() - bufferAt, totalLen - accumulatedLength);
             HBuffer data = referenceBuffer.SubBuffer(bufferAt, bufferLen);
