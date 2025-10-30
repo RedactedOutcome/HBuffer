9d7e66a06331b5e10c5bdc486b198a0c817e5909
The1WeirdDev <the1weirddev@gmail.com>
2025-10-29 21:13:03 -0500

    Fixed pch

--- include/HBuffer/Core.h
+++ include/HBuffer/Core.h
@@ -1,13 +1,6 @@
 #pragma once
 
 //Handles defines and importing standard library
-#ifdef HBUFFER_USE_PCH
-#ifndef HBUFFER_PCH_DIR
-    #error HBUFFER no pch location specified
-#endif
-
-#include HBUFFER_PCH_DIR
-#else
 #include <iostream>
 #include <string>
 #include <string.h>
@@ -15,8 +8,6 @@
 #include <memory>
 #include <algorithm>
 #include <vector>
-
-#endif
 //#if __cplusplus < 201103L
 //#error HBUFFER ONLY SUPPORTS CPP11+ remind the dev to actually work on his code 
 //#endif
